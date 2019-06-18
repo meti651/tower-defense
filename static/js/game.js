56ba126069f74c39ab6d62227b3025025a32c3b0
Bence Kovács <kovacs.ben.p@gmail.com>
2019-06-18 11:47:02 +0200

    drag and dropping almost works

--- static/js/game.js
+++ static/js/game.js
@@ -1,3 +1,25 @@
+function dragstart_handler(ev) {
+ // Add the target element's id to the data transfer object
+    ev.dataTransfer.setData("text/plain", ev.target.id);
+    ev.dataTransfer.dropEffect = "copy";
+}
+
+function dragover_handler(ev) {
+    ev.preventDefault();
+ // Set the dropEffect to move
+    ev.dataTransfer.dropEffect = "copy"
+}
+
+
+function drop_handler(ev) {
+    ev.preventDefault();
+ // Get the id of the target and add the moved element to the target's DOM
+    let data = ev.dataTransfer.getData("text/plain");
+    let newNode = document.getElementById(data).cloneNode(true);
+    newNode.id = "newId";
+
+    ev.target.appendChild(newNode);
+}
 
 
 function main() {

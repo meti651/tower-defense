cdd07da464b7ce105dead06a09dc304fa5c7b4ee
meti651 <meti651@gmail.com>
2019-06-18 11:54:50 +0200

    nothing happened

--- static/js/game.js
+++ static/js/game.js
@@ -1,11 +1,8 @@
-
-
 function main() {
     let inventoryCells = document.querySelectorAll(".inventory-cell");
     for (let i = 0; i < inventoryCells.length; i++) {
         inventoryCells[i].textContent = i;
     }
-
 
 
 }
